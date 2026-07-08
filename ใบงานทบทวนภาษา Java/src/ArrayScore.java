d6d82f0a2c00d31e063053e4f913811662b1d508
016 ธนากร สุธากมลรัตน์ <s68122250016@ssru.ac.th>
2026-07-09 00:22:50 +0700

    Add files via upload

---
+++ ใบงานทบทวนภาษา Java/src/ArrayScore.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class ArrayScore {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int[] scores = new int[5];
+        int total = 0;
+        double average;
+
+        for (int i = 0; i < scores.length; i++) {
+            System.out.print("Enter score " + (i + 1) + ": ");
+            scores[i] = sc.nextInt();
+            total += scores[i];
+        }
+
+        average = (double) total / scores.length;
+
+        System.out.println("Total score = " + total);
+        System.out.println("Average score = " + average);
+
+        sc.close();
+    }
+}
